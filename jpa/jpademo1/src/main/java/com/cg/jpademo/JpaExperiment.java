5e45ceb1097e925a0cd2bdf8c331244b03e56f5f
vineetsemwal <vineetsemwal82@gmail.com>
2021-05-11 15:02:36 +0530

    crud operations demo added

--- jpa/jpademo1/src/main/java/com/cg/jpademo/JpaExperiment.java
+++ jpa/jpademo1/src/main/java/com/cg/jpademo/JpaExperiment.java
@@ -23,12 +23,30 @@ public class JpaExperiment {
         Employee prajakta=new Employee("prajakta", 30000);
         EntityTransaction transaction =entityManager.getTransaction();
         transaction.begin();
-        entityManager.persist(krishna);// will insert as row in table
-        entityManager.persist(prajakta);
+        entityManager.persist(krishna);// will insert a row in table
+        entityManager.persist(prajakta);// will insert a row in table
+
         transaction.commit();
         System.out.println("details for krishna inserted with id="+krishna.getId());
         System.out.println("details for prajakta inserted with id="+prajakta.getId());
+        int prajaktaId=prajakta.getId();
+        Employee fetched=entityManager.find(Employee.class,prajaktaId);
+        System.out.println("fetched object details="+fetched.getId()+" -"+fetched.getName()+"-"+fetched.getBalance());
 
+         transaction=entityManager.getTransaction();
+         transaction.begin();
+         prajakta.setBalance(50000);
+         prajakta.setName("prajakta deshmukh");
+         //
+        //merge has 2 options , one is updating previously existing row another is inserting new row if
+        // primary field of object does not exist in the table
+        //
+         entityManager.merge(prajakta);
+         transaction.commit();
+         System.out.println("balance updated for prajakta");
+
+
+        emf.close();
     }
 
 
