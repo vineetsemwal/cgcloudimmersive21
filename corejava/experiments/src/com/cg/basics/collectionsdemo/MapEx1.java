ed12bea26d808c60f9e8384666c3f85ed8c58876
vineetsemwal <vineetsemwal82@gmail.com>
2021-05-14 18:51:48 +0530

    string keys used in map

--- corejava/experiments/src/com/cg/basics/collectionsdemo/MapEx1.java
+++ corejava/experiments/src/com/cg/basics/collectionsdemo/MapEx1.java
@@ -5,23 +5,24 @@ import java.util.*;
 public class MapEx1 {
 
     public static void main(String[] args){
-        Map<Integer,Customer>map=new HashMap<>();
-        Customer customer1=new Customer("priti",21);
-        Customer customer2=new Customer("prajakta",22);
-        Customer customer3=new Customer("maniratnam",23);
-        Customer customer4=new Customer("raju",23);
+        Map<String,Customer>map=new HashMap<>();
+        String name1="priti",name2="prajakta",name3="maniratnam",name4="raju";
+        Customer customer1=new Customer(name1,21);
+        Customer customer2=new Customer(name2,22);
+        Customer customer3=new Customer(name3,23);
+        Customer customer4=new Customer(name4,23);
 
-        map.put(10,customer1);
-        map.put(2,customer2);
-        map.put(3,customer3);
-        map.put(2,customer4);
-        Customer fetched=map.get(2);
+        map.put(customer1.getName(),customer1);
+        map.put(customer2.getName(),customer2);
+        map.put(customer3.getName(),customer3);
+        map.put(customer4.getName(),customer4);
+        Customer fetched=map.get(name2);
         System.out.println("customer with key 2 "+fetched.getName()+"-"+fetched.getAge());
         int size=map.size();
         System.out.println("size="+size);
         System.out.println("iterating on keys and fetching values");
-        Set<Integer>keys=map.keySet();
-       for (Integer key:keys){
+        Set<String>keys=map.keySet();
+       for (String key:keys){
           Customer customer= map.get(key);
            System.out.println(customer.getName()+"-"+customer.getAge());
        }
